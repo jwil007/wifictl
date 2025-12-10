d275d1784a0ae981192efb83ffbd4f10a0c127ab
Jacob Wilson <jacobtwilson7@gmail.com>
2025-12-10 15:28:10 -0700

    update scratch bin for testing connection

--- cmd/scratch/main.go
+++ cmd/scratch/main.go
@@ -6,11 +6,26 @@ import (
 	"github.com/jwil007/wifictl/internal/connect"
 )
 
-const iface = "wlp0s20f3"
+const (
+	iface = "wlp0s20f3"
+	ssid  = "Get off my LAN"
+)
 
 func main() {
-	err := connect.Connect(iface)
+	var conn connect.WiFiConnection
+	conn.Base.SSID = ssid
+	conn.Base.ConName = ssid
+	conn.Base.Iface = iface
+	conn.Security = connect.PSKSec{
+		Passphrase: "Fargo1234",
+		SAE:        false,
+	}
+	uuid, err := connect.RunNmcliConnAdd(conn)
 	if err != nil {
-		fmt.Printf("Error happened: %v", err)
+		fmt.Printf("Error adding ssid: %s", err)
+	}
+	err1 := connect.RunNmcliConnUp(uuid)
+	if err1 != nil {
+		fmt.Printf("Error connecting: %s", err)
 	}
 }
